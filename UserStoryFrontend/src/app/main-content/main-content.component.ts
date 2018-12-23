127a18d30802a8051ff5f6b6371d23923e2ea169
Rakshan Thapar <rakshanthapar32@gmail.com>
2018-12-23 23:31:02 +0530

    minor modifications to the look and feel of the page

--- UserStoryFrontend/src/app/main-content/main-content.component.ts
+++ UserStoryFrontend/src/app/main-content/main-content.component.ts
@@ -23,6 +23,39 @@ export class MainContentComponent implements OnInit {
     {
       description: 'Work',
       updated: new Date('1/28/16'),
+    }, {
+      description: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      description: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      description: 'Work',
+      updated: new Date('1/28/16'),
+    }, {
+      description: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      description: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      description: 'Work',
+      updated: new Date('1/28/16'),
+    }, {
+      description: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      description: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      description: 'Work',
+      updated: new Date('1/28/16'),
     }
   ];
 
