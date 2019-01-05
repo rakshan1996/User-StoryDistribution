2ca55ec6cddbb41426ca2fb0b847e47283621b7d
Rakshan Thapar <rakshanthapar32@gmail.com>
2019-01-06 00:44:41 +0530

    minor modification in the look of the page

--- UserStoryFrontend/src/app/app.component.ts
+++ UserStoryFrontend/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material';
-import { MatDialog } from '@angular/material';
-import { AddStoryComponent } from './add-story/add-story.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +8,4 @@ import { AddStoryComponent } from './add-story/add-story.component';
 })
 export class AppComponent {
   title = 'UserStory';
-  constructor(private dialog: MatDialog){}
-  openDialog(){
-    this.dialog.open(AddStoryComponent);
-  }
 }
